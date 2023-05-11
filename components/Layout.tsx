f332d6ffe86973f0551d8afe61faf1c28db046d6
Savvy <savvywang@gmail.com>
2023-05-11 15:36:35 +0800

    update the setup

---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+interface LayoutProps{
+    children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({children}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Layout
